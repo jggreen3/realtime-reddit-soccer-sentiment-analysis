c7235719f2f3ff493b0fdc4223796d778eb6b2b5
John-The-Fisherman <jackgreen4372@gmail.com>
2024-08-19 14:45:16 -0700

    updated lambda deployment script to create package directory

--- deployment_scripts/deploy_lambda.sh
+++ deployment_scripts/deploy_lambda.sh
@@ -7,14 +7,21 @@ LAMBDA_FUNCTION_NAME=$1
 ROLE=$2
 ZIP_FILE="lambda_function.zip"
 
-# Package the lambda function comment table into a zip file
-cd ../src/processing/package
-zip -r ../$ZIP_FILE .
-cd ..
-zip -r $ZIP_FILE lambda_handler.py
-zip -r $ZIP_FILE comment_table.py
+# Step 1: Install boto3 into a directory called 'package'
+mkdir -p ../src/processing/package
+pip install --target ../src/processing/package boto3
 
+# Step 2: Package the 'package' directory into a zip file
+cd ../src/processing/package
+zip -r9 ../$ZIP_FILE .
+
+# Step 3: Add the lambda function files to the zip file
+cd ..
+zip -g $ZIP_FILE lambda_handler.py
+zip -g $ZIP_FILE comment_table.py
+
+# Step 4: Deploy the Lambda function
 aws lambda create-function --function-name $LAMBDA_FUNCTION_NAME --zip-file fileb://$ZIP_FILE \
-    --handler lambda_handler.lambda_handler --runtime python3.8 --role $2
+    --handler lambda_handler.lambda_handler --runtime python3.8 --role $ROLE
 
 echo "Lambda function deployed successfully."
